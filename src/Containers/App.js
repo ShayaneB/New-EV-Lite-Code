8e987fd8002e7e987f643491994ff3746e63c799
ShayaneB <shayane.basu@eisenvault.com>
2020-07-01 16:09:22 +0530

    updated

---
+++ src/Containers/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './styles.scss';
+
+import Routings from './Routings';
+
+const App = () => {
+  return (
+    <div>
+      <Routings />
+    </div>
+  )
+};
+
+export default App;
